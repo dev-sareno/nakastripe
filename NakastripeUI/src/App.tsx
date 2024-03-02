b71ee48baa4b2dad5d9d8e8e1bd36880df5db296
dev-sareno <dev.sareno@gmail.com>
2024-03-02 19:59:07 +0800

    chore(ui): Remove Stripe hook

--- NakastripeUI/src/App.tsx
+++ NakastripeUI/src/App.tsx
@@ -8,7 +8,6 @@ import {
 } from "react-router-dom";
 import ProductsPage from './Pages/ProductsPage/ProductsPage';
 import PaymentPage from './Pages/PaymentPage/PaymentPage';
-import { StripeProvider } from './Hooks/UseStripe';
 
 const router = createBrowserRouter([
   {
@@ -19,17 +18,15 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <StripeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="payment/:id" element={<PaymentPage />} />
-          <Route path="callback/payment/success" element={<div>Payment successful!</div>} />
-          <Route path="callback/payment/cancelled" element={<div>Payment cancelled!</div>} />
-          <Route path="/" element={<ProductsPage />} />
-          <Route path="*" element={<div>Page not found</div>} />
-        </Routes>
-      </BrowserRouter>
-    </StripeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="payment/:id" element={<PaymentPage />} />
+        <Route path="callback/payment/success" element={<div>Payment successful!</div>} />
+        <Route path="callback/payment/cancelled" element={<div>Payment cancelled!</div>} />
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="*" element={<div>Page not found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
